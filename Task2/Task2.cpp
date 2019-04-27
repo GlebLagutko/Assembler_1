f9f85ff0c3f5a4f5e7bf320626b9b15d41f973ab
Gleb Lagutko <gleblagutko@gmail.com>
2019-04-27 14:43:17 +0300

    Ready

--- Task2/Task2.cpp
+++ Task2/Task2.cpp
@@ -3,7 +3,10 @@
 
 using namespace std;
 
-void SecondTask(int  a, int b)
+const float S = 2;
+const float Z = 1;
+
+float SecondTask(float  a, float b)
 {
 	float x;
 	__asm
@@ -12,20 +15,50 @@ void SecondTask(int  a, int b)
 		fldz
 		fsub b
 		fdiv a
-		fstp x
+		fst x
 	}
-	wcout << L"Answer : " << x << endl;
+	wcout << L"Second task : " << x << endl;
+	return x;
+}
+
+void ThirdTask(float x)
+{
+	float result;
+	float x1;
+	float x2;
+	__asm
+	{
+		finit
+		fldz
+		fld S
+		fsub
+		fld Z
+		fmul
+		fld x
+		fadd
+		fld S
+		fld Z
+		fmul
+		fld x
+		fadd
+		fdiv
+		fld S
+		fmul
+
+		fstp result
+	}
+	wcout << "Third task : " << result << endl;
 }
 
 int main()
 {
-	int a;
-	int b;
+	float a;
+	float b;
 	wcout << L"A = ";
 	wcin >> a;
 	wcout << L"B = ";
 	wcin >> b;
-	SecondTask(a, b);
-
+	float x = SecondTask(a, b);
+	ThirdTask(x);
 	system("pause");
 }
